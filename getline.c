1e19aa2537ac59b39eebe9682da988ee4d97c797
Esllam-Saeed <108064795+Esllam-Saeed@users.noreply.github.com>
2023-04-26 21:28:45 +0200

    Update getline.c

--- getline.c
+++ getline.c
@@ -1,123 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+#include "shell.h"
 
 /**
- * _realloc - Reallocates a memory block using malloc and free.
- * @ptr: A pointer to the memory previously allocated.
- * @old_size: The size in bytes of the allocated space for ptr.
- * @new_size: The size in bytes for the new memory block.
- *
- * Return: If new_size == old_size - ptr.
- *         If new_size == 0 and ptr is not NULL - NULL.
- *         Otherwise - a pointer to the reallocated memory block.
+ * _getline - getline function.
+ * @buffer: input in shell.
+ * @size: size input.
+ * @fp: file pointer.
+ * Return: Always 0.
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+int64_t _getline(char **buffer, size_t *size, FILE *fp)
 {
-    void *mem;
-    char *ptr_copy, *filler;
-    unsigned int index;
+	char chars[1024];
 
-
-    if (new_size == old_size)
-        return (ptr);
-
-
-    if (ptr == NULL)
-    {
-        mem = malloc(new_size);
-        if (mem == NULL)
-            return (NULL);
-
-
-        return (mem);
-    }
-
-
-    if (new_size == 0 && ptr != NULL)
-    {
-        free(ptr);
-        return (NULL);
-    }
-
-
-    ptr_copy = ptr;
-    mem = malloc(sizeof(*ptr_copy) * new_size);
-    if (mem == NULL)
-    {
-        free(ptr);
-        return (NULL);
-    }
-
-
-    filler = mem;
-
-
-    for (index = 0; index < old_size && index < new_size; index++)
-        filler[index] = *ptr_copy++;
-
-
-    free(ptr);
-    return (mem);
+	if (buffer == NULL || size == NULL || fp == NULL)
+	{
+		fputs("WRONG ARGUMENTS.\n", stderr);
+		return (-1);
+	}
+	if (*buffer == NULL)
+	{
+		*size = sizeof(chars);
+		*buffer = malloc(*size);
+		if (*buffer == NULL)
+		{
+			perror("Unable to allocate memory for buffer");
+			return (-1);
+		}
+	}
+	(*buffer)[0] = '\0';
+	while (fgets(chars, sizeof(chars), fp) != NULL)
+	{
+		if (*size - strlen(*buffer) < sizeof(chars))
+		{
+			*size *= 2;
+			*buffer = realloc(*buffer, *size);
+			if (*buffer == NULL)
+			{
+				perror("Unable to re-allocate memory for buffer");
+				free(buffer);
+				return (-1);
+			}
+		}
+		strcat(*buffer, chars);
+		if ((*buffer)[strlen(*buffer) - 1] == '\n')
+			return (strlen(*buffer));
+	}
+	return (-1);
 }
-
-
-/**
- * assign_lineptr - Reassigns the lineptr variable for _getline.
- * @lineptr: A buffer to store an input string.
- * @n: The size of lineptr.
- * @buffer: The string to assign to lineptr.
- * @b: The size of buffer.
- */
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
-{
-    if (*lineptr == NULL)
-    {
-        if (b > 120)
-            *n = b;
-        else
-            *n = 120;
-        *lineptr = buffer;
-    }
-    else if (*n < b)
-    {
-        if (b > 120)
-            *n = b;
-        else
-            *n = 120;
-        *lineptr = buffer;
-    }
-    else
-    {
-        strcpy(*lineptr, buffer);
-        free(buffer);
-    }
-}
-
-
-/**
- * _getline - Reads input from a stream.
- * @lineptr: A buffer to store the input.
- * @n: The size of lineptr.
- * @stream: The stream to read from.
- *
- * Return: The number of bytes read.
- */
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
-{
-    static ssize_t input;
-    ssize_t ret;
-    char c = 'x', *buffer;
-    int r;
-
-
-    if (input == 0)
-        fflush(stream);
-    else
-        return (-1);
-    input = 0
