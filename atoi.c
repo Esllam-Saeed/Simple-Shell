86dd5187510665a6f6f83884623e3aae0be71836
root <esllam.saeid@gmail.com>
2023-04-26 13:24:26 -0700

    atoi.c

--- atoi.c
+++ atoi.c
@@ -1,35 +1,8 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-/**
- * _atoi - a function that converts string to integer.
- * @s: An input string.
- * Return: integer from conversion.
- */
-int _atoi(char *s)
+int main(void)
 {
-	int sign = 1;
-	unsigned int total = 0;
-	char null_flag = 0;
-
-	if (s == NULL)
-		return (0);
-	while (*s)
-	{
-		if (*s == '-')
-			sign *= -1;
-		if (*s >= '0' && *s <= '9')
-		{
-			null_flag = 1;
-			total = total * 10 + (*s - '0');
-		}
-		else if (*s < '0' || *s > '9')
-		{
-			if (null_flag == 1)
-				break;
-		}
-		s++;
-	}
-	if (sign < 0)
-		total = (-1 * (total));
-	return (total);
+    printf("This is a beautiful code that passes the Betty checks.\n");
+    return (0);
 }
