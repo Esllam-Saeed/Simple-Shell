0090cfd89ffa411be8b28bb7f7c6e1227b0f3b77
ihaythamadel <123040960+ihaythamadel@users.noreply.github.com>
2023-04-26 21:07:45 +0200

    Update getline.c

--- getline.c
+++ getline.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _getline - read the input from a stream, alwyas STDIN
+ * _getline - read the input from a stream, always STDIN
  * @lineptr: buffer where the input is stored
  * @sz: size of input buffer
  * @thread: the input file handler/stream
@@ -16,23 +16,26 @@ ssize_t _getline(char **lineptr, size_t *sz, FILE *thread)
 	int readBool;
 	ssize_t res;
 
-	if (input == 0)
-		fflush(thread);
-	else
+	if (input != 0)
 		return (-1);
 
+	fflush(thread);
 	input = 0;
+
 	buf = malloc(sizeof(char) * 120);
 	if (!buf)
 		return (-1);
+
 	while (c != '\n')
 	{
 		readBool = read(STDIN_FILENO, &c, 1);
+
 		if (readBool == -1 || (readBool == 0 && input == 0))
 		{
 			free(buf);
 			return (-1);
 		}
+
 		if (readBool == 0 && input != 0)
 		{
 			input++;
@@ -40,14 +43,18 @@ ssize_t _getline(char **lineptr, size_t *sz, FILE *thread)
 		}
 
 		if (input >= 120)
-			buf = _realloc(buf, input, input + 1);
+			buf = _realloc(buf, 120, input + 1);
+
 		buf[input] = c;
 		input++;
 	}
+
 	buf[input] = '\0';
 	reassign_pr(lineptr, sz, buf, input);
 	res = input;
+
 	if (readBool != 0)
 		input = 0;
+
 	return (res);
 }
